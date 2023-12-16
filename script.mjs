f1e3a01a87bd83ee6ed06b895f40d9574b84efcb
Saba Saeed khan <144582707+saeedsamina@users.noreply.github.com>
2023-12-17 01:30:51 +0500

    events

---
+++ script.mjs
@@ -0,0 +1,29 @@
+
+function greetings() {
+    alert('welcome')
+}
+
+function greetings() {
+    alert('welcome')
+}
+
+function greetUser(username) {
+    document.write(`welcome to my page: ${username}`)
+}
+
+
+function adding(a, b) {
+    document.write(`the sum of ${a} , ${b} is ${a + b}`)
+}
+
+function findsum(arr) {
+    let sum = 0
+    for (a = 0; a < arr.length; a++) {
+        sum += arr[a]
+
+    }
+    document.write(`the array is ${arr} <br>
+    the sum of all the numbers in the array is ${sum}`)
+}
+
+
